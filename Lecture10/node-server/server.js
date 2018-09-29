87f375ee7628dd1e6f9534b3e9a2206dc8308196
Abhishek Yadav <abhishekgit1208@gmail.com>
2018-09-29 17:14:03 +0530

    updated lecture10
    Signed-off-by: Abhishek Yadav <abhishekgit1208@gmail.com>

--- Lecture10/node-server/server.js
+++ Lecture10/node-server/server.js
@@ -7,13 +7,19 @@ srv.get('/', (req, res, next) => {
     res.send("Hello World");
 });
 
-srv.get('/abc',(req,res)=>{
+srv.get('/abc', (req, res) => {
     res.send("The request came to abc")
 })
 srv.get('/abc/:id', (req, res, next) => {
     res.send("The id is " + req.params.id);
 });
 
+srv.get('/html', (req, res, next) => {
+    res.send(`<html>
+<h1>Welcome to my site</h1>
+<i>Please login!</i>
+</html>`)
+})
 
 srv.listen(8080, () => {
     console.log("The server is up and running at 8080!")
